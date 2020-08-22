70c1c011a271063e3c106f7c3607c0515c93f315
stefanZorcic <70064280+stefanZorcic@users.noreply.github.com>
2020-08-22 10:25:10 -0400

    Update neural-network.py

--- neural-network.py
+++ neural-network.py
@@ -1,2 +1,20 @@
-print("hi")
+# Recurrent Neural Network
+# Goal to have sentimet recognizition
+
+#Modlues
+import numpy as np
+import random
+import matplotlib.pyplot as plt
+  
+def sigmoid(x):
+  return 1.0/(1.0+np.exp(x))
+
+#Chain rule = dError/dw = dError/douto * douto/dino * dino/dw
+
+def derivative1(outo, targetOutput):
+  return outo-targetOutput
+
+def derivative2(outo):
+  return outo*(1-outo)
+  
 
