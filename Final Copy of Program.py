73e873eb51eca14f73fc98ca01109432a94f2a93
stefanZorcic <70064280+stefanZorcic@users.noreply.github.com>
2020-08-23 20:04:22 -0400

    Update Final Copy of Program.py

--- Final Copy of Program.py
+++ Final Copy of Program.py
@@ -1,14 +1,14 @@
 import csv
+temp=[0]*0
+X = [0]*0
+Y = [0]*0
 
 u=0
 
-temp = [0] * 0
-X = [0] * 0
-Y = [0] * 0
+inter = 10
 
-inter = 200
+train = 5 # Number of how much of data is test data
 
-train = 100  # Number of how much of data is test data
 
 with open('training_data.csv') as csvDataFile:
     csvReader = csv.reader(csvDataFile)
@@ -27,30 +27,16 @@ nltk.download('punkt')
 nltk.download('stopwords')
 nltk.download('product_reviews_1')
 
+stop_words=set(stopwords.words('english'))
 
-def find(L, target):
-    start = 0
-    end = len(L) - 1
-
-    while start <= end:
-        middle = (start + end) / 2
-        midpoint = L[int(middle)]
-        if midpoint > target:
-            end = middle - 1
-        elif midpoint < target:
-            start = middle + 1
-        else:
-            return midpoint
-
-
-stop_words = set(stopwords.words('english'))
+word_list = pro.words()
 
 for i in range(inter):
-    indices = [0] * 0
+    indices=[0]*0
     # Text preprocessing
-    text = str(temp[i + 1][2])
+    text = str(temp[i+1][2])
 
-    if str(temp[i + 1][3]) == "1":
+    if str(temp[i+1][3])=="1":
         Y.append(1)
     else:
         Y.append(0)
@@ -73,36 +59,29 @@ for i in range(inter):
 
     text = filtered_sentence
 
-    # text = nltk.word_tokenize(text)
-
-
-    word_list = pro.words()
-    word_list = sorted(word_list)
     indices = ['0.01'] * 73833
 
-    # print(text)
 
-
-    z = 0
+    z=0
 
     for i in range(int(len(text))):
         try:
-            o = (find(word_list, text[i]))
+            o = (word_list.index(text[i]))
             q = int(indices[o])
-            q += 1.0 * i;
-            indices.pop(int(0 - 1))
-            indices.insert(int(o - 1), str(q))
-            q = 0
+            q+=1.0*i;
+            indices.pop(int(0-1))
+            indices.insert(int(o-1),str(q))
+            q=0
 
-        except TypeError:
-            indices.pop(73833 - 1)
-            z += 1.0 * i
+        except ValueError:
+            indices.pop(73833-1)
+            z+=1.0*i
             indices.append(str(z))
 
     #print(len(indices))
-    # indices = np.array(indices)
-    # print(indices)
-    # print(indices)
+    #indices = np.array(indices)
+    #print(indices)
+    #print(indices)
 
     u+=1
     print(u)
@@ -116,26 +95,24 @@ Y = np.array(Y)
 
 print(Y)
 
-print(Y)
-
 from sklearn.neural_network import MLPClassifier
-from sklearn.metrics import classification_report, confusion_matrix, accuracy_score
+from sklearn.metrics import classification_report,confusion_matrix,accuracy_score
 
-classifier = MLPClassifier(hidden_layer_sizes=(20, 30),
-                           activation='tanh',
+classifier = MLPClassifier(hidden_layer_sizes=(10,2),
+                           activation='relu',
                            solver='lbfgs',
                            alpha=0.05,
                            batch_size='auto',
                            learning_rate='adaptive',
-                           learning_rate_init=0.1,
+                           learning_rate_init=0.05,
                            power_t=0.5,
-                           max_iter=50,
+                           max_iter=5000,
                            shuffle=True,
                            random_state=None,
-                           tol=0.0001,
+                           tol=0.01,
                            verbose=False,
                            warm_start=False,
-                           momentum=0.9,
+                           momentum=0.3,
                            nesterovs_momentum=True,
                            early_stopping=False,
                            validation_fraction=0.1,
@@ -143,88 +120,120 @@ classifier = MLPClassifier(hidden_layer_sizes=(20, 30),
                            beta_2=0.999,
                            epsilon=1e-08,
                            n_iter_no_change=10,
-                           max_fun=1500)
+                           max_fun=15000)
 
-counter = 0
+counter=0
 
-'''
-s=True
-while s:
-    for i in range(100):
-        X_train=X[i]
-        X_train=X_train.astype(np.float64)
-        #print(X_train)
-        print(counter,classifier.fit([X_train], [Y[i]]))
-        counter+=1;
-    if(input()=="x"):
-        s=False
-'''
-
-X_train = X
-# X_train=X_train.astype(np.float64)
-# print(X_train)
-
+X_train=X
 print(X_train)
 print(Y)
 
-Y = np.array(Y)
+Y=np.array(Y)
 Y.shape
 print(Y)
 
 classifier.fit(X_train, Y)
-counter += 1;
+counter+=1;
 print("YYYAYYAYAYYAYAYYA")
+
+
 
 num = 0
 dem = 0
 
-for i in range(inter - train):
-    X_Test = X[i + train]
-    X_Test = X_Test.astype(np.float64)
-    # print(str(classifier.predict([X_Test])) + str(Y[i+int(train)]))
-    if (int(classifier.predict([X_Test]))) == int(Y[i + int(train)]):
-        num += 1
-    dem += 1
+for i in range(inter-train):
+    X_Test=X[i+train]
+    X_Test=X_Test.astype(np.float64)
+    #print(str(classifier.predict([X_Test])) + str(Y[i+int(train)]))
+    if (int(classifier.predict([X_Test]))) == int(Y[i+int(train)]):
+        num+=1
+    dem+=1
 
-    print((int(classifier.predict([X_Test]))), int(Y[i + int(train)]))
+    print((int(classifier.predict([X_Test]))), int(Y[i+int(train)]))
 
-print(str(num / dem * 100) + "% Accuracy")
+print(str(num/dem*100)+"% Accuracy")
 
-'''
-p=100;
-data=[0]*0
-with open('training_data.csv') as csvDataFile:
+temp=[0]*0
+X = [0]*0
+
+u=0
+
+
+with open('contestant_judgment.csv') as csvDataFile:
     csvReader = csv.reader(csvDataFile)
     for row in csvReader:
-        data.append(row)
-for i in range(inter):
+        temp.append(row)
+
+nltk.download('words')
+nltk.download('punkt')
+nltk.download('stopwords')
+nltk.download('product_reviews_1')
+
+stop_words=set(stopwords.words('english'))
+
+for i in range(10):
     indices=[0]*0
     # Text preprocessing
-    text = str(data[i+1][2])
+    text = str(temp[i+1][2])
+
     text = text.lower()
     remove_digits = str.maketrans('', '', string.digits)
     text = text.translate(remove_digits)
     text = text.translate(str.maketrans('', '', string.punctuation))
     text = text.strip()
-    text = nltk.word_tokenize(text)
-    word_list = words.words()
-    indices = ['0.01'] * 236737
+
+    word_tokens = nltk.word_tokenize(text)
+
+    filtered_sentence = [w for w in word_tokens if not w in stop_words]
+
+    filtered_sentence = []
+
+    for w in word_tokens:
+        if w not in stop_words:
+            filtered_sentence.append(w)
+
+    text = filtered_sentence
+
+    word_list = pro.words()
+    indices = ['0.01'] * 73833
+
     z=0
+
     for i in range(int(len(text))):
         try:
             o = (word_list.index(text[i]))
             q = int(indices[o])
-            q+=1*i;
+            q+=1.0*i;
             indices.pop(int(0-1))
             indices.insert(int(o-1),str(q))
             q=0
-        except ValueError:
-            indices.pop(236737-1)
-            z+=1*i
-            indices.append(str(z))
-    indices = np.array(indices)
-    data.append(indices)
-for i in range(p):
-    (int(classifier.predict([X_Test])))
 
-'''
+        except ValueError:
+            indices.pop(73833-1)
+            z+=1.0*i
+            indices.append(str(z))
+
+    u+=1
+    print("Loading: " + str(u))
+
+    indices = np.array(indices)
+    indices = indices.astype(np.float64)
+
+    X.append(indices)
+
+from csv import writer
+from csv import reader
+
+for i in range(inter-train):
+    X_Test=X[i]
+    X_Test=X_Test.astype(np.float64)
+
+    ans = int(classifier.predict([X_Test]))
+
+    with open('contestant_judgment1.csv', 'r') as read_obj, \
+            open('output_1.csv', 'w', newline='') as write_obj:
+        csv_reader = reader(read_obj)
+        csv_writer = writer(write_obj)
+        for row in csv_reader:
+            row.append(ans)
+            csv_writer.writerow(row)
